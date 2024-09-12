3e4a034d86aa136eb88a00f8bbda36e6b499bc5a
Ravinder Singh <ravindersngh402@outlook.com>
2024-09-12 10:01:21 +0530

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # crud-bean-validation
+-Reference Blog post - https://snyk.io/blog/guide-to-input-validation-with-spring-boot/
---
+++ src/main/java/com/ravinder/project/simple/crud/SimpleCrudBeanValidationApplication.java
@@ -0,0 +1,13 @@
+package com.ravinder.project.simple.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudBeanValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudBeanValidationApplication.class, args);
+	}
+
+}
