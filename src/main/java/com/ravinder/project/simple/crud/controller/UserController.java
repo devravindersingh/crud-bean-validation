fb841462295bde80d806abdee5c3438b0e3fd282
Ravinder Singh <ravindersngh402@outlook.com>
2024-09-12 12:08:10 +0530

    added base url to controller with validation annotation

--- src/main/java/com/ravinder/project/simple/crud/controller/UserController.java
+++ src/main/java/com/ravinder/project/simple/crud/controller/UserController.java
@@ -5,12 +5,15 @@ import com.ravinder.project.simple.crud.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
+@Validated
 public class UserController {
     @Autowired
     private UserRepository userRepository;
